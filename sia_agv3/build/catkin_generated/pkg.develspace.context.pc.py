0aa42f25358e0806706b2270d84f1f8e4257c05f
cheng9911 <1962945668@qq.com>
2022-07-20 10:19:36 +0800

    修改雷达扫描范围不关于小车前方对称

---
+++ sia_agv3/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "sia_agv3"
+PROJECT_SPACE_DIR = "/home/sun/robot/sia/src/ROS_siasun_rboot/sia_agv3/build/devel"
+PROJECT_VERSION = "0.0.0"
---
+++ sia_agv3/build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/sun/robot/sia/src/ROS_siasun_rboot/sia_agv3/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
