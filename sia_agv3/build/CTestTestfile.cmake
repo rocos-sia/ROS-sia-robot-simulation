7b61cfeab8cb3b70e0cfa22f211e4f86391507df
cheng9911 <1962945668@qq.com>
2022-07-20 10:20:25 +0800

    修改雷达扫描范围不关于小车前方对称

--- sia_agv3/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sun/robot/sia/src/ROS_siasun_rboot/sia_agv3
-# Build directory: /home/sun/robot/sia/src/ROS_siasun_rboot/sia_agv3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- sia_agv3/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/sun/robot/sia/src/ROS_siasun_rboot/sia_agv3/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
